f21a4923f1a62c3b1d1ff57fb936c79e7b41f7cc
Oleksii Kapula <kon2vpalto@gmail.com>
2024-06-06 10:55:08 +0300

    Change tags

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,25 +1,22 @@
-/* See LICENSE file for copyright and license details. */
+static const unsigned int borderpx = 2;
+static const unsigned int snap     = 32;
+static const int showbar           = 1;
+static const int topbar            = 1;
+static const char *fonts[]         = { "Iosevka Nerd Font:size=12" };
+static const char dmenufont[]      = "Iosevka Nerd Font:size=12";
 
-/* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka Nerd Font:size=12" };
-static const char dmenufont[]       = "Iosevka Nerd Font:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const char col_gray1[] = "#222222";
+static const char col_gray2[] = "#444444";
+static const char col_gray3[] = "#bbbbbb";
+static const char col_gray4[] = "#eeeeee";
+static const char col_cyan[]  = "#005577";
+
+static const char *colors[][3] = {
+  [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+  [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
-/* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "idx", "web", "cli", "dev", "vbx", "rdn", "cht", "vid", "otr" };
 
 static const Rule rules[] = {
 	/* xprop(1):
